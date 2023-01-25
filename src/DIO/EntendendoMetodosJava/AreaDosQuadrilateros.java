ebff9d44da351d90dfeb443a486bd805f7fea7a9
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2023-01-25 15:56:29 -0300

    Updated DIO Entendendo Métodos JAVA -AreaDosQuadrilateros utilizando métodos com return

--- src/DIO/EntendendoMetodosJava/AreaDosQuadrilateros.java
+++ src/DIO/EntendendoMetodosJava/AreaDosQuadrilateros.java
@@ -1,21 +1,15 @@
 package DIO.EntendendoMetodosJava;
 
 public class AreaDosQuadrilateros {
-    public static void area(double lado){
-        System.out.println("Área do quadrado é " + (lado*lado));
-    }
+    public static double area(double lado){ return lado * lado;}
 
-    public static void area(double largura, double comprimento){
-        System.out.println("Área do retângulo é " + (largura*comprimento));
-    }
+    public static double area(double largura, double comprimento){ return largura * comprimento;}
 
-    public static void area(double baseMaior, double baseMenor, double altura){
-        System.out.println("Área do trapézio é " + ((baseMaior+baseMenor)*altura/2));
-    }
+    public static double area(double baseMaior, double baseMenor, double altura){ return (baseMaior+baseMenor)*altura/2;}
 
     public static void main(String[] args){
-        AreaDosQuadrilateros.area(6.0);
-        AreaDosQuadrilateros.area(2.0,5.5);
-        AreaDosQuadrilateros.area(10.0,5.0,2.5);
+        System.out.println("Área do quadrado é " + AreaDosQuadrilateros.area(6.0));
+        System.out.println("Área do retângulo é " + AreaDosQuadrilateros.area(2.0,5.5));
+        System.out.println("Área do trapézio é " + AreaDosQuadrilateros.area(10.0,5.0,2.50));
     }
 }
