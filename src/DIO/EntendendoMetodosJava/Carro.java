d84b59d68219d2ed2979189d0ffb5b51dff236eb
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2023-01-26 16:24:47 -0300

    Updated DIO Programação Orientada a Objetos -Carro

--- src/DIO/EntendendoMetodosJava/Carro.java
+++ src/DIO/EntendendoMetodosJava/Carro.java
@@ -4,12 +4,41 @@ public class Carro {
     String cor;
     String modelo;
     int capacidadeTanque;
+
     Carro(){}
-    Carro(String cor, String modelo){
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        setCor(cor);
+        setModelo(modelo);
+        setCapacidadeTanque(capacidadeTanque);
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+    void setCor(String cor) {
         this.cor = cor;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setModelo(String modelo) {
         this.modelo = modelo;
     }
-    void calcularValorTotal(float valorGasolina){
-        System.out.println(this.capacidadeTanque*valorGasolina);
+
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+
+    double totalValorTanque(float valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
     }
 }
