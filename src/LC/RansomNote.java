9f781fa989e6a6acf70fff84b6b995f3e57736f6
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2023-01-31 18:10:26 -0300

    Added LC 383. Ransom Note First attempt It didn't work

---
+++ src/LC/RansomNote.java
@@ -0,0 +1,49 @@
+package LC;
+
+// 383. Ransom Note
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RansomNote {
+    public static boolean canConstruct() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type ransomNote");
+        String ransomNote = scanner.nextLine();
+        System.out.println("Type magazine");
+        String magazine = scanner.nextLine();
+
+        HashMap base = new HashMap();
+        //return magazine.contains(ransomNote);
+
+        for (int i = 0; i < magazine.length(); i++){
+            int ammount = 1;
+            if (base.containsKey(magazine.charAt(i))){
+                ammount = (int) base.get(magazine.charAt(i));
+                base.replace(magazine.charAt(i),ammount++);
+            }
+            else base.put(magazine.charAt(i),ammount);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++){
+            int ammount = -1;
+            if (base.containsKey(ransomNote.charAt(i))){
+                ammount = (int) base.get(ransomNote.charAt(i));
+                base.replace(ransomNote.charAt(i),ammount--);
+            }
+            else base.put(ransomNote.charAt(i),ammount);
+        }
+
+        boolean answer = true;
+
+        for (Object value: base.values()){
+            if ((int) value < 0) answer = false;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RansomNote.canConstruct());
+    }
+
+}
